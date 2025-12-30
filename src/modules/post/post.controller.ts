e0073b2c1258fc4e61e972a08be51fccab407e2f
bbbbmo <bbbbmo@github.com>
2025-12-30 17:58:05 +0900

    ✨feat: 게시글 id 기반 조회 기능 추가

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -1,13 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { Post } from './entities/post.entity';
 import { PostService } from './post.service';
 
-@Controller()
+@Controller('post')
 export class PostController {
   constructor(private readonly postService: PostService) {}
 
   @Get()
   async getPosts(): Promise<Post[]> {
     return await this.postService.getAllPosts();
+  }
+
+  @Get(':id')
+  async getPostById(@Param('id') id: number): Promise<Post | null> {
+    return await this.postService.getPostById(id);
   }
 }
