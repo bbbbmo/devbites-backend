7dd3c5def77fe927447a7533b7f4a12c14fb63eb
bbbbmo <aqw20501@naver.com>
2026-02-12 22:39:40 +0900

    ➕chore: dotenv 추가/firebase 사용 시 env 파일 별도로 로드하도록 변경

--- src/firebase-admin.ts
+++ src/firebase-admin.ts
@@ -1,4 +1,9 @@
 import * as admin from 'firebase-admin';
+import { config as loadEnv } from 'dotenv';
+
+// dotenv 타입이 좁게 지정되지 않아 no-unsafe-call 경고가 발생해 예외 처리
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+loadEnv({ path: '.env.local' });
 
 admin.initializeApp({
   credential: admin.credential.cert({
