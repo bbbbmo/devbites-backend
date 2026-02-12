3f40ae77d3013c36450f21dfef9adde9caee0712
bbbbmo <aqw20501@naver.com>
2026-02-12 22:39:57 +0900

    🐛fix: 임포트 에러 수정

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { FirebaseAuthService } from './auth.service';
+import { FirebaseAuthGuard } from './guards/auth.guard';
 
 @Module({
   controllers: [AuthController],
-  providers: [FirebaseAuthService],
-  exports: [FirebaseAuthService],
+  providers: [FirebaseAuthService, FirebaseAuthGuard],
+  exports: [FirebaseAuthService, FirebaseAuthGuard],
 })
 export class AuthModule {}
 
--- src/modules/subcription/subscription.module.ts
+++ src/modules/subcription/subscription.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Subscription } from './entities/subscription.entity';
 import { SubscriptionService } from './subscription.service';
 import { SubscriptionController } from './subscription.controller';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Subscription])],
+  imports: [TypeOrmModule.forFeature([Subscription]), AuthModule],
   controllers: [SubscriptionController],
   providers: [SubscriptionService],
 })
